4a59a88e31f102b70796ebb5adac61d4899adb2a
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-04 12:37:57 +0600

    first class advanced completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+}
+
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+// createFareMultiplier higher order function
+function createFareMultiplier(multiplier){
+    return function (fare) {
+        return fare * multiplier;
+    }
+};
+
+// fareDoubler
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);
+}
